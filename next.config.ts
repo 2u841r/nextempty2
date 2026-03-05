0ceb6b6eb281bf3f44f2ffd7db673240b8ebf482
Zubair Ibn Zamir <zizdjbd@gmail.com>
2026-03-05 14:57:02 +0600

    fixxx

--- next.config.ts
+++ next.config.ts
@@ -4,12 +4,12 @@ const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default nextConfig;
 
 // import('@opennextjs/cloudflare').then(m => m.initOpenNextCloudflareForDev());
 
-// for fix
-
-if (process.env.NODE_ENV === 'development' && !process.env.CI && !process.env.VERCEL) {
-  await import('@opennextjs/cloudflare').then(m => m.initOpenNextCloudflareForDev());
+if (process.env.NODE_ENV === 'development' && !process.env.VERCEL) {
+  // fire-and-forget, no await at top level
+  import('@opennextjs/cloudflare').then(m => m.initOpenNextCloudflareForDev());
 }
+
+export default nextConfig;
